ce3d36230765ad82b9c0b2296d84bd88a3133e40
lucassherwin <lksherwin@gmail.com>
2022-02-05 20:44:54 -0500

    adds text input

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
 
 const SignIn = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View>
       <Text>Sign In</Text>
+      <View>
+        <TextInput onChangeText={setUsername} style={styles.input} value={username} />
+        <TextInput onChangeText={setPassword} style={styles.input} value={password} />
+      </View>
       <Text style={styles.link} onPress={() => navigation.navigate('SignUp')}>Don't have an account? Click here to sign up</Text>
     </View>
   );
@@ -15,5 +22,11 @@ export default SignIn;
 const styles = StyleSheet.create({
   link: {
     color: 'blue'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
   }
 });
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
   return (
     <View>
       <Text>Sign Up</Text>
+      <View>
+        <TextInput onChangeText={setUsername} style={styles.input} value={username} />
+        <TextInput onChangeText={setPassword} style={styles.input} value={password} />
+        <TextInput onChangeText={setEmail} style={styles.input} value={email} />
+        <TextInput onChangeText={setFirstName} style={styles.input} value={firstName} />
+        <TextInput onChangeText={setLastName} style={styles.input} value={lastName} />        
+      </View>
+      <Text style={styles.link} onPress={() => navigation.navigate('SignUp')}>Don't have an account? Click here to sign up</Text>
     </View>
   );
 };
 
 export default SignUp;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  link: {
+    color: 'blue'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
+  }
+});
